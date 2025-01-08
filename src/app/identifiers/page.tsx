ed70b659130ad060ae8d52193cd15d51bd43d3bb
Kian-MH <kian.groenen@mediahuis.be>
2025-01-08 18:49:59 +0100

    test

--- src/app/identifiers/page.tsx
+++ src/app/identifiers/page.tsx
@@ -5,23 +5,7 @@ import Identifiers from "./components/Identifiers";
 
 const Page = () => {
   const tableName = 'YourDynamoDBTable';
-  const identifiersPromise = scan(tableName).then(res => res.json())
-  .then(data => {
-    // TODO: replace with proper API fetch
-    // Working with single table for all records for testing purposes, only SizeMapping has a name property, using this to filter
-    // @ts-ignore
-    const identifiers = data.filter(item => item.value);
-    // @ts-ignore
-    const entityGroups = data.filter(item => item.group);
-
-    // @ts-ignore
-    const options = entityGroups.map(item => {return { const: item.id, title: item.group }});
-    const identifierSchema = inCompleteIdentifierSchema;
-
-    identifierSchema['properties']['entityGroup']['oneOf'] = options;
-
-    return { identifiers, identifierSchema };
-  });
+  const identifiersPromise = new Promise(resolve => { return resolve({ identifiers: [], identifierSchema: inCompleteIdentifierSchema }) });
   return (
     <>
       <div id='wrapper'>
