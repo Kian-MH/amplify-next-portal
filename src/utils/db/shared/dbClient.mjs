7ec1f80705c9840c6a84fc50d061055ab85d720a
Kian-MH <kian.groenen@mediahuis.be>
2025-01-09 11:29:48 +0100

    test

--- src/utils/db/shared/dbClient.mjs
+++ src/utils/db/shared/dbClient.mjs
@@ -4,6 +4,10 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 // TODO: remove region? (used for testing locally)
 const awsConfig = {
   region: 'eu-west-1',
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
+  }
 }
 
 const dbClient = new DynamoDBClient(awsConfig);
